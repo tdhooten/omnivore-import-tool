fa63dfe2d8dd026c1397687657fb931e169fc9d8
tdhooten <github.ejszn@simplelogin.com>
2023-11-02 21:55:49 -0400

    Update namespaces.

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Omnivore_Importer.Pages;
+namespace OmnivoreImportTool.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
