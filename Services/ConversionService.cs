1dd47cc5f82e0848726de286025e5894e4eeea4b
tdhooten <github.ejszn@simplelogin.com>
2023-11-02 23:51:54 -0400

    Refactor to more easily support other import formats.

--- Services/ConversionService.cs
+++ Services/ConversionService.cs
@@ -6,15 +6,33 @@ namespace OmnivoreImportTool.Services;
 
 public class ConversionService
 {
-    public Stream ConvertRaindrop(Stream stream)
+    public Stream ConvertFile(Stream stream, string inputFormat)
     {
-        List<OmnivoreModel> outputRecords = new();
+        var outputRecords = Enumerable.Empty<OmnivoreModel>();
 
         stream.Position = 0;
         using var reader = new StreamReader(stream);
         using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
         var records = csvReader.GetRecords<RaindropModel>();
 
+        switch (inputFormat)
+        {
+            case "Raindrop":
+                outputRecords = ConvertRaindrop(records);
+                break;
+        }
+
+        using var outputStream = new MemoryStream();
+        using var writer = new StreamWriter(outputStream);
+        using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        csvWriter.WriteRecords(outputRecords);
+        csvWriter.Flush();
+
+        return new MemoryStream(outputStream.ToArray());
+    }
+
+    private static IEnumerable<OmnivoreModel> ConvertRaindrop(IEnumerable<RaindropModel> records)
+    {
         foreach (var record in records)
         {
             var output = new OmnivoreModel()
@@ -29,15 +47,7 @@ public class ConversionService
 
             if (record.folder.Contains("archive", StringComparison.InvariantCultureIgnoreCase)) output.state = "ARCHIVED";
 
-            outputRecords.Add(output);
+            yield return output;
         }
-
-        using var outputStream = new MemoryStream();
-        using var writer = new StreamWriter(outputStream);
-        using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
-        csvWriter.WriteRecords(outputRecords);
-        csvWriter.Flush();
-
-        return new MemoryStream(outputStream.ToArray());
     }
 }
